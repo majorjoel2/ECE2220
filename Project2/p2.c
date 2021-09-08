780ff4482b4a98bd486ea4ad5ecf92119225d9c8
Joel Tanner <jftanne@g.clemson.edu>
2021-09-08 12:02:48 -0400

    Add includes and struct todo change default and ask if having a default global is ok

---
+++ Project2/p2.c
@@ -0,0 +1,26 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct idiot {
+  char[3] prefix;
+  char[32] firstName;
+  char middleInitial;
+  char[32] lastName;
+  char[3] suffix;
+  int age;
+  char sex;
+  char[32] officeHeld;
+  int yearsServed;
+  char[3] politicalParty;
+  char[2] homeState;
+  char[15] twitter;
+  char[13] phoneNumber;
+} idiot_default = '0';
+
+
+int main(int arg, char **argv){
+  return 1;
+}
