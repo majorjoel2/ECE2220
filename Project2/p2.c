d11f27f79ec22927817255d38750677ce9398240
Joel Tanner <jftanne@g.clemson.edu>
2021-09-09 18:48:32 -0400

    Add dono and wealth make set to deafult function to clear a politician

--- Project2/p2.c
+++ Project2/p2.c
@@ -18,9 +18,32 @@ struct idiot {
   char[2] homeState;
   char[15] twitter;
   char[13] phoneNumber;
-} idiot_default = '0';
+  double donationsReceived;
+  long double totalWealth;
+};
 
+idiot setToDefaultPolitician();
 
 int main(int arg, char **argv){
   return 1;
+}
+
+idiot setToDefaultPolitician(){
+  idiot default_politician;
+  default_politician.prefix = '-';
+  default_politician.firstName = '-';
+  default_politician.middleInitial = '-';
+  default_politician.lastName = '-';
+  default_politician.suffix = '-';
+  default_politician.age = -1;
+  default_politician.sex = '-';
+  default_politician.officeHeld = '-';
+  default_politician.yearsServed = -1;
+  default_politician.politicalParty = '-';
+  default_politician.homeState = '-';
+  default_politician.twitter = '-';
+  default_politician.phoneNumber = '-';
+  default_politician.donationsReceived = -0.1;
+  default_politician.totalWealth = -0.1;
+  return default_politician;
 }
