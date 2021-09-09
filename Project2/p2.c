bba876249cff2f96dbf0316907d30cb54594c4bf
Joel Tanner <jftanne@g.clemson.edu>
2021-09-09 19:11:03 -0400

    bugfix, add state and abbrev programmer tired and write bad code :(

--- Project2/p2.c
+++ Project2/p2.c
@@ -5,44 +5,65 @@
 #include <string.h>
 
 struct idiot {
-  char[3] prefix;
-  char[32] firstName;
+  char prefix[3];
+  char firstName[32];
   char middleInitial;
-  char[32] lastName;
-  char[3] suffix;
+  char lastName[32];
+  char suffix[3];
   int age;
   char sex;
-  char[32] officeHeld;
+  char officeHeld[32];
   int yearsServed;
-  char[3] politicalParty;
-  char[2] homeState;
-  char[15] twitter;
-  char[13] phoneNumber;
+  char politicalParty[3];
+  char homeState[2];
+  char twitter[15];
+  char phoneNumber[13];
   double donationsReceived;
   long double totalWealth;
 };
 
-idiot setToDefaultPolitician();
+struct idiot setToDefaultPolitician();
+
+char statesName[50][15] = {
+  "Alabama", "Alaska", "Arizona", "Arkansas", "California",
+  "Colorado", "Connecticut", "Delaware", "Florida", "Georgia",
+  "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
+  "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland",
+  "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri",
+  "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey",
+  "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio",
+  "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
+  "South Dakota", "Tennessee", "Texas", "Utah", "Vermont",
+  "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
+};
+
+char stateAbbreviations[50][3] = {
+  "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA",
+  "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+  "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+  "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+  "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"
+};
 
 int main(int arg, char **argv){
   return 1;
 }
 
-idiot setToDefaultPolitician(){
-  idiot default_politician;
-  default_politician.prefix = '-';
-  default_politician.firstName = '-';
+struct idiot setToDefaultPolitician(){
+  struct idiot default_politician;
+  default_politician.prefix[0] = '-';
+  default_politician.firstName[0] = '-';
   default_politician.middleInitial = '-';
-  default_politician.lastName = '-';
-  default_politician.suffix = '-';
+  default_politician.lastName[0] = '-';
+  default_politician.suffix[0] = '-';
   default_politician.age = -1;
   default_politician.sex = '-';
-  default_politician.officeHeld = '-';
+  default_politician.officeHeld[0] = '-';
   default_politician.yearsServed = -1;
-  default_politician.politicalParty = '-';
-  default_politician.homeState = '-';
-  default_politician.twitter = '-';
-  default_politician.phoneNumber = '-';
+  default_politician.politicalParty[0] = '-';
+  default_politician.homeState[0] = '-';
+  default_politician.twitter[0] = '-';
+  default_politician.phoneNumber[0] = '-';
   default_politician.donationsReceived = -0.1;
   default_politician.totalWealth = -0.1;
   return default_politician;
