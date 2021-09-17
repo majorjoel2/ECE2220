eb2a3c0822217416d119ca872cb617197e8a6982
Joel Tanner <jftanne@g.clemson.edu>
2021-09-17 17:25:55 -0400

    add read, set, and clear bit functions

--- Project3/p3.c
+++ Project3/p3.c
@@ -7,8 +7,26 @@
 #include <string.h>
 
 
+byte readBit(char byte, char bitPosition);
+void setBit(char *byte, char bitPosition);
+void clearBit(char *byte, char bitPosition);
 
 
 int main(int arg, char **argv){
   return 1;
+}
+
+
+byte readBit(char byte, char bitPosition){
+  bitPosition == 1 << bitPosition;
+  return (byte & bitPosition);
+}
+void setBit(char *byte, char bitPosition){
+  bitPosition == 1 << bitPosition;
+  *byte = (*byte | bitPosition);
+}
+void clearBit(char *byte, char bitPosition){
+  bitPosition == 1 << bitPosition;
+  bitPosition ^= 0xff;
+  *byte = (*byte & bitPosition);
 }
