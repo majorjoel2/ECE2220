321c5ba1ed635abafcd5585d6f04ad76e9a83942
Joel Tanner <jftanne@g.clemson.edu>
2021-09-17 17:36:50 -0400

    bug fixes, add union to access data bytes, start generateParity function

--- Project3/p3.c
+++ Project3/p3.c
@@ -6,10 +6,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+union dataBytes {
+  long number;
+  char bytes[8];
+};
 
-byte readBit(char byte, char bitPosition);
+char readBit(char byte, char bitPosition);
 void setBit(char *byte, char bitPosition);
 void clearBit(char *byte, char bitPosition);
+char generateParity(char inputData[8]);
 
 
 int main(int arg, char **argv){
@@ -17,16 +22,16 @@ int main(int arg, char **argv){
 }
 
 
-byte readBit(char byte, char bitPosition){
-  bitPosition == 1 << bitPosition;
+char readBit(char byte, char bitPosition){
+  bitPosition = 1 << bitPosition;
   return (byte & bitPosition);
 }
 void setBit(char *byte, char bitPosition){
-  bitPosition == 1 << bitPosition;
+  bitPosition = 1 << bitPosition;
   *byte = (*byte | bitPosition);
 }
 void clearBit(char *byte, char bitPosition){
-  bitPosition == 1 << bitPosition;
+  bitPosition = 1 << bitPosition;
   bitPosition ^= 0xff;
   *byte = (*byte & bitPosition);
 }
