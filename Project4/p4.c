7c8534670add34c80885d8334349c371acacaf94
Joel Tanner <jftanne@g.clemson.edu>
2021-10-06 13:24:41 -0400

    Add input conversion

--- Project4/p4.c
+++ Project4/p4.c
@@ -22,8 +22,28 @@ char encode[10][26] = {
 void encodeLetter(char *message, int keyNum, int offset, int currentIndex);
 void decodeLetter(char *message, int keyNum, int offset, int currentIndex);
 
-int main(int arg, char **argv){
+int main(int argc, char *argv[]){
+  int i = 0, j = 0, offsetVal;
+  for(i = 0; i < argc; i++){
+    printf("arg[%i] = %s\tLen:%li\n", i, argv[i], strlen(argv[i]));
+  }
   int keyOrder[10] = {0,1,2,3,4,5,6,7,8,9};
+  char messageInput[31];
+  for(i = 0, j = 0; i < strlen(argv[2]); i++){
+    if(argv[2][i] >= 48 && argv[2][i] <= 57){
+      //number
+      if(argv[2][i] == 48){
+        //10
+        keyOrder[j] = 9;
+      } else {
+        keyOrder[j] = argv[2][i] - '0' - 1;
+      }
+    } else {
+      j++;
+    }
+  }
+  sscanf(argv[3], "%i", &offsetVal);
+  strcpy(messageInput, argv[4]);
   return 1;
 }
 
