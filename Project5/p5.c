b448cc2c955742e02a81777a75f97100ac1a0e58
Joel Tanner <jftanne@g.clemson.edu>
2021-10-26 20:30:29 -0400

    Add testing for dict open and input open. add func for num of lines

--- Project5/p5.c
+++ Project5/p5.c
@@ -5,6 +5,30 @@
 #include <stdlib.h>
 #include <string.h>
 
+int numberOfLines(FILE *inputFile);
+
 int main(int argc, char *argv[]){
-  return 1;
+  char dictLocation[64] = "/usr/share/dict/";
+  printf("Dictionary: %p\n", fopen(strcat(dictLocation, argv[1]), "r"));
+  FILE *inputFile = fopen(argv[2], "r");
+  printf("Input File: %p\n", inputFile);
+  printf("1: %li\t", ftell(inputFile));
+  int nol = numberOfLines(inputFile);
+  printf("4: %li\n", ftell(inputFile));
+  printf("NOL: %i\n", nol);
+  return 0;
+}
+
+int numberOfLines(FILE *inputFile){
+  int output = 0;
+  char c;
+  printf("2: %li\t", ftell(inputFile));
+  for(c = getc(inputFile); c != EOF; c = getc(inputFile)){
+    if(c == '\n'){
+      output++;
+    }
+  }
+  printf("3: %li\t", ftell(inputFile));
+  rewind(inputFile);
+  return output;
 }
