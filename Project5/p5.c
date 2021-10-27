a7471cfecd9ca8d619c236ce268f1d6dc73d48de
Joel Tanner <jftanne@g.clemson.edu>
2021-10-27 13:59:57 -0400

    add write to file. TODO add dump mem func

--- Project5/p5.c
+++ Project5/p5.c
@@ -11,6 +11,7 @@ int lengthOfDict(FILE *inputFile);
 int dictBinarySearch(char **dictionary, int *wordlen, int first, int last, char *word);
 void loadMemFromFile(FILE *fileToLoad, char **memPointer, int numOfLines, int *lineLengths);
 int searchFileByLine(char **file, int *lens, int start, int end, int *lineFound, int *charFound, char *word);
+void writeMemToFile(FILE *fileToSave, char **memPointer, int numOfLines, int *lineLengths);
 
 int main(int argc, char *argv[]){
   char **lines, **dict;
@@ -49,6 +50,11 @@ int main(int argc, char *argv[]){
   loadMemFromFile(inputFile, lines, nol, len);
   loadMemFromFile(dictFile, dict, dictl, dictLen);
 
+  FILE *outputFile;
+  outputFile = fopen(argv[3], "w");
+  writeMemToFile(outputFile, lines, nol, len);
+  fclose(outputFile);
+
   /*printf("Dict 102384: ");
   for(i = 0; i < dictLen[102400]; i++){
     printf("%c", dict[102400][i]);
@@ -66,11 +72,11 @@ int main(int argc, char *argv[]){
 
   //printf("Search for A: %i\n", dictBinarySearch(dict, dictLen, 0, dictl-1, "Sus"));
 
-  int fLN = 0, fCOL = 0;
+  /*int fLN = 0, fCOL = 0;
   printf("wordLen: %i\n", searchFileByLine(lines, len, 9, nol, &fLN, &fCOL, "to"));
   printf("ln: %i\tcol: %i\n", fLN, fCOL);
   printf("wordLen: %i\n", searchFileByLine(lines, len, fLN, nol, &fLN, &fCOL, "to"));
-  printf("ln: %i\tcol: %i\n", fLN, fCOL);
+  printf("ln: %i\tcol: %i\n", fLN, fCOL);*/
 
   return 0;
 }
@@ -178,7 +184,7 @@ int searchFileByLine(char **fileSearch, int *lens, int start, int end, int *line
     if(start == *lineFound && *charFound != 0){
       char *subString;
       subString = (char *)memcpy((void *)subString, (void *)(&fileSearch[start][*charFound+wordLength]), lens[start]-*charFound);
-      printf("Total: %s\nSub:%s", fileSearch[start], subString);
+      //printf("Total: %s\nSub:%s", fileSearch[start], subString);
       found = strstr(subString, word);
       if(found){
         printf("Found %i, %p, %p\n", i, found, subString);
@@ -196,4 +202,21 @@ int searchFileByLine(char **fileSearch, int *lens, int start, int end, int *line
     }
   }
   return -1;
+}
+
+void writeMemToFile(FILE *fileToSave, char **memPointer, int numOfLines, int *lineLengths){
+  rewind(fileToSave);
+  int i, j;
+  char c;
+  for(i = 0; i < numOfLines; i++){
+    for(j = 0; j < lineLengths[i]; j++){
+      c = memPointer[i][j];
+      if(c == 10){
+        putc(13, fileToSave);
+      }
+      putc(c, fileToSave);
+      //printf("c:i:j:mem\t%i:%i:%i:%i\n", c, i, j, memPointer[i][j]);
+      //printf("%c", memPointer[i][j]);
+    }
+  }
 }
