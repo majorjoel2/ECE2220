a9e69d593a56737a1173a1668cf39b13db321b73
Joel Tanner <jftanne@g.clemson.edu>
2021-10-26 20:44:40 -0400

    added length of line and tested

--- Project5/p5.c
+++ Project5/p5.c
@@ -6,6 +6,7 @@
 #include <string.h>
 
 int numberOfLines(FILE *inputFile);
+int lengthOfLine(FILE * inputFile);
 
 int main(int argc, char *argv[]){
   char dictLocation[64] = "/usr/share/dict/";
@@ -16,6 +17,12 @@ int main(int argc, char *argv[]){
   int nol = numberOfLines(inputFile);
   printf("4: %li\n", ftell(inputFile));
   printf("NOL: %i\n", nol);
+  int i, *len;
+  len = (int *)malloc(nol*sizeof(int));
+  for(i = 0; i < nol; i++){
+    len[i] = lengthOfLine(inputFile);
+    printf("Line Len %i = %i\n", i, len[i]);
+  }
   return 0;
 }
 
@@ -30,5 +37,15 @@ int numberOfLines(FILE *inputFile){
   }
   printf("3: %li\t", ftell(inputFile));
   rewind(inputFile);
+  return output;
+}
+
+int lengthOfLine(FILE * inputFile){
+  int output = 0;
+  char c;
+  for(c = getc(inputFile); c != '\n'; c = getc(inputFile)){
+    output++;
+  }
+  //c = getc(inputFile);
   return output;
 }
