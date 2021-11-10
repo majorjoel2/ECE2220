b570eebff296c509151c9ceb195bdbebe82740d9
Joel Tanner <jftanne@g.clemson.edu>
2021-11-10 13:07:58 -0500

    Add comments

--- Project5/p5.c
+++ Project5/p5.c
@@ -6,6 +6,8 @@
  *
  * Known Issues
  * Spell check doesn't save new words or check new dictionary
+ * search and replace: if too much is replaced results in "malloc(): corrupted top size"
+ * save terminates the program because of memory problems
  */
 
 //Libraries used in this program
@@ -42,35 +44,39 @@ void replaceWord(char **memPointer, int *lineLengths, int lineIndex, int charInd
 int dictLinearSearch(char **dictionary, int *wordlen, int first, int last, char *word);
 
 int main(int argc, char *argv[]){
-
+  //Validate args
   if(argc != 4){
     printf("Wrong number of args\n");
     return 1;
   }
   char **lines, **dict;
-
+  //open dict
   char dictLocation[64] = "/usr/share/dict/";
   FILE *dictFile = fopen(strcat(dictLocation, argv[3]), "r");
   if(dictFile == NULL){
     printf("ERROR: Failed to load Dictionary\n");
     return 1;
   }
+  //open file
   FILE *inputFile = fopen(argv[1], "r");
   if(inputFile == NULL) {
     printf("ERROR: Failed to load Input File\n");
     return 1;
   }
 
+  //allocate mem for dict and file
   int fileNOL = 0, *fileLineLengths, dictNOL = 0, *dictLineLengths;
   allocateMem(inputFile, &lines, &fileNOL, &fileLineLengths);
   allocateDict(dictFile, &dict, &dictNOL, &dictLineLengths);
   printf("NOL: %i:%i\n", fileNOL, dictNOL);
 
+  //load files into allocated memory
   loadMemFromFile(inputFile, lines, fileNOL, fileLineLengths);
   loadMemFromFile(dictFile, dict, dictNOL, dictLineLengths);
 
   int exitLoop = 0;
   while(exitLoop == 0){
+    //get option
     printf("[SP]Spell check\n[SE]Search\n[SR]Search and Replace\n[SA]Save\n[EX]Exit\n");
     char optionInput[32];
     scanf("%s", optionInput);
@@ -84,6 +90,7 @@ int main(int argc, char *argv[]){
         //printf("line:%i,len:%i\n", line, length);
         //printf("dict:(%i,%c)\n", lines[line][length],lines[line][length]);
         if(isalpha(lines[line][length]) != 0){
+          //add letter
           //printf("is alpha %c\n", lines[line][length]);
           spellingWord[word] = lines[line][length];
           word++;
@@ -104,6 +111,7 @@ int main(int argc, char *argv[]){
         length++;
         if(length >= fileLineLengths[line]){
           if(line+1 >= fileNOL){
+            //check last word as well
             spellingWord[word] = 0;
             if(dictBinarySearch(dict, dictLineLengths, 0, dictNOL, spellingWord) == -1){
               printf("Word not found %s at line %i with index %i\n", spellingWord, line, length);
@@ -131,6 +139,7 @@ int main(int argc, char *argv[]){
       while(dummyClear != 10){
         scanf("%c", &dummyClear);
       }
+      //get string input
       printf("Enter string to search: ");
       scanf("%c", &currentLetter);
       if(currentLetter != 10){
@@ -164,6 +173,7 @@ int main(int argc, char *argv[]){
       if(lengthOfFoundWord == -1){
         printf("None found!\n");
       } else {
+        //search until no more found
         while(lengthOfFoundWord != -1){
           printf("Found string at line: %i with index: %i and length: %i\nThe line is: %s", foundLine+1, foundIndex, lengthOfFoundWord, lines[foundLine]);
           lengthOfFoundWord = searchFileByLine(lines, fileLineLengths, foundLine, fileNOL, &foundLine, &foundIndex, searchString);
@@ -171,7 +181,7 @@ int main(int argc, char *argv[]){
       }
     }
     if(tolower(optionInput[0]) == 's' && tolower(optionInput[1]) == 'r'){
-      //Search
+      //Search and replace
       char searchString[65];
       char replaceString[65];
       char currentLetter, dummyClear = 0;
@@ -179,6 +189,7 @@ int main(int argc, char *argv[]){
       while(dummyClear != 10){
         scanf("%c", &dummyClear);
       }
+      //get input string to search
       printf("Enter string to search: ");
       scanf("%c", &currentLetter);
       if(currentLetter != 10){
@@ -206,6 +217,7 @@ int main(int argc, char *argv[]){
         printf("Bad/no Input\n");
         return 1;
       }
+      //get input string to replace
       printf("Enter string to replace: ");
       scanf("%c", &currentLetter);
       if(currentLetter != 10){
@@ -239,15 +251,18 @@ int main(int argc, char *argv[]){
       if(lengthOfFoundWord == -1){
         printf("None found!\n");
       } else {
+        //go until no more found
         while(lengthOfFoundWord != -1){
           printf("Found string at line: %i with index: %i and length: %i\nThe line is: %s", foundLine+1, foundIndex, lengthOfFoundWord, lines[foundLine]);
           lengthOfFoundWord = searchFileByLine(lines, fileLineLengths, foundLine, fileNOL, &foundLine, &foundIndex, searchString);
+          //replace and print new line
           replaceWord(lines, fileLineLengths, foundLine, foundIndex, lengthOfFoundWord, replaceString);
           printf("New line is: %s\n", lines[foundLine]);
         }
       }
     }
     if(tolower(optionInput[0]) == 's' && tolower(optionInput[1]) == 'a'){
+      //save and exit because saving kills memory
       FILE *outputFile;
       outputFile = fopen(argv[2], "w");
       writeMemToFile(outputFile, lines, fileNOL, fileLineLengths);
@@ -266,12 +281,15 @@ int numberOfLines(FILE *inputFile){
   int output = 0;
   char c, cLast;
   //printf("2: %li\t", ftell(inputFile));
+  //read the entire file
   for(c = getc(inputFile); c != EOF; c = getc(inputFile)){
+    //count the number of newlines
     if(c == '\n'){
       output++;
     }
     cLast = c;
   }
+  //if the last one isn't a newline add one to total
   if(cLast != '\n'){
     output++;
   }
@@ -283,6 +301,7 @@ int numberOfLines(FILE *inputFile){
 int lengthOfLine(FILE * inputFile){
   int output = 0;
   char c;
+  //reads current line and counts number of chars
   for(c = getc(inputFile); c != '\n' && c != EOF; c = getc(inputFile)){
     output++;
   }
@@ -297,6 +316,7 @@ int lengthOfDict(FILE * inputFile){
     output++;
   }
   //c = getc(inputFile);
+  //same as lengthOfLine but preincrement output because dict format
   return ++output;
 }
 
@@ -327,8 +347,12 @@ int dictBinarySearch(char **dictionary, int *wordlen, int first, int last, char 
 
     //printf("SearchingB %i/%i/%s\n", middle, strcmp(wordLower, dictWordLower), dictWordLower);
 
+    //This line is the cause of my misery because if the word is shorter than
+    //the words around it then if can't find the word so I shortened the dict
+    //to strlen(word) when I make the dictionary lowercase
     if(strcmp(wordLower, dictWordLower) == 0){
       free(dictWordLower);
+      //call linear search to find the short words
       return dictLinearSearch(dictionary, wordlen, first, middle+1, wordLower);
     }
 
@@ -352,6 +376,7 @@ int dictLinearSearch(char **dictionary, int *wordlen, int first, int last, char 
   for(curLine = first; curLine < last; curLine++){
     dictWordLower = (char *)malloc(wordlen[curLine]*sizeof(char));
     if(dictWordLower == NULL) printf("ERROR: Failed to allocate dictWordLower\n");
+    //This time its reading in the whole word
     for(i = 0; i < wordlen[curLine]; i++){
       dictWordLower[i] = tolower(dictionary[curLine][i]);
     }
@@ -360,8 +385,11 @@ int dictLinearSearch(char **dictionary, int *wordlen, int first, int last, char 
     //if(curLine == first) printf("SearchingL %i/%i/%s/%s\n", curLine, strcmp(word, dictWordLower), dictWordLower, word);
     //printf("SearchingL %i/%i/%s/%s\n", curLine, strcmp(word, dictWordLower), dictWordLower, word);
 
+    //This works because the for loop is searching sequentially
     if(strcmp(word, dictWordLower) == 0){
       free(word);free(dictWordLower);
+      //it does a double return after freeing the dict word from this function
+      //and the word allocated in the binary search function
       return curLine;
     }
 
@@ -379,6 +407,7 @@ void loadMemFromFile(FILE *fileToLoad, char **memPointer, int numOfLines, int *l
   for(i = 0; i < numOfLines; i++){
     for(j = 0; j < lineLengths[i]; j++){
       c = getc(fileToLoad);
+      //ignore CRs (files have 13 10, dict has 10 only)
       if(c == 13){
         c = getc(fileToLoad);
       }
@@ -391,12 +420,14 @@ void loadMemFromFile(FILE *fileToLoad, char **memPointer, int numOfLines, int *l
 }
 
 int searchFileByLine(char **fileSearch, int *lens, int start, int end, int *lineFound, int *charFound, char *word){
+  //file isn't sorted :( so must search linearly
   int wordLength = strlen(word);
   int i;
   char *found;
   for(i = start; i < end; i++){
     found = strstr(fileSearch[i], word);
     if(i == *lineFound && *charFound != 0){
+      //make a substring to read the rest of the line that has previously been searched
       char *subString;
       subString = (char *)malloc((lens[start]-*charFound)*sizeof(char));
       memcpy(subString, (&fileSearch[start][*charFound+wordLength]), lens[start]-*charFound);
@@ -407,6 +438,7 @@ int searchFileByLine(char **fileSearch, int *lens, int start, int end, int *line
         *lineFound = i;
         *charFound = ((int)(found-subString)) + *charFound + wordLength;
         free(subString);
+        //line and index are passed by refrence so return wordlength for replace func
         return wordLength;
       }
       free(subString);
@@ -419,16 +451,19 @@ int searchFileByLine(char **fileSearch, int *lens, int start, int end, int *line
       }
     }
   }
+  //return a negative value to indicate none found
   return -1;
 }
 
 void writeMemToFile(FILE *fileToSave, char **memPointer, int numOfLines, int *lineLengths){
+  //rewind the file as a precaution
   rewind(fileToSave);
   int i, j;
   char c;
   for(i = 0; i < numOfLines; i++){
     for(j = 0; j < lineLengths[i]; j++){
       c = memPointer[i][j];
+      //add back in that pesky CR
       if(c == 10){
         putc(13, fileToSave);
       }
@@ -441,6 +476,7 @@ void writeMemToFile(FILE *fileToSave, char **memPointer, int numOfLines, int *li
 
 void dumpMem(char ***memPointer, int numOfLines, int **lineLengths){
   int i;
+  //free everything in reverse allocation order
   for(i = 0; i < numOfLines; i++){
     free(*memPointer[i]);
   }
@@ -450,6 +486,7 @@ void dumpMem(char ***memPointer, int numOfLines, int **lineLengths){
 
 void allocateMem(FILE *openFile, char ***memPointer, int *numOfLines, int **lineLengths){
   int i;
+  //allocate memory for all the pointers
   *numOfLines = numberOfLines(openFile);
   *lineLengths = (int *)malloc(*numOfLines*sizeof(int));
   if(*lineLengths == NULL) printf("ERROR: Failed to allocate lineLengths\nCode: %p\n", openFile);
@@ -465,6 +502,7 @@ void allocateMem(FILE *openFile, char ***memPointer, int *numOfLines, int **line
 
 void allocateDict(FILE *openFile, char ***memPointer, int *numOfLines, int **lineLengths){
   int i;
+  //same as allocateMem but uses lengthOfDict instead of lengthOfLine
   *numOfLines = numberOfLines(openFile);
   *lineLengths = (int *)malloc(*numOfLines*sizeof(int));
   if(*lineLengths == NULL) printf("ERROR: Failed to allocate lineLengths\nCode: %p\n", openFile);
@@ -484,17 +522,24 @@ void replaceWord(char **memPointer, int *lineLengths, int lineIndex, int charInd
   char *newLine = (char *)malloc(newLineLength*sizeof(char));
   if(newLine == NULL) printf("ERROR: Failed to allocate newLine\n");
   lineLengths[lineIndex] = newLineLength;
+  //makes two substrings
   char *subString, *subString2;
   subString = (char *)malloc(charIndex*sizeof(char));
   if(subString == NULL) printf("ERROR: Failed to allocate subString\n");
+  //one for all before the old word
   subString = (char *)memcpy((void *)subString, (void *)memPointer[lineIndex], charIndex);
   strcpy(newLine, subString);
+  //add in the new word
   strcat(newLine, newWord);
+  //the second for everything after the old word
   subString2 = (char *)malloc((newLineLength - charIndex - strlen(newWord))*sizeof(char));
   if(subString2 == NULL) printf("ERROR: Failed to allocate subString2\n");
   subString2 = (char *)memcpy((void *)subString2, (void *)(&memPointer[lineIndex][charIndex + oldWordLength]), newLineLength - charIndex - strlen(newWord));
   strcat(newLine, subString2);
+  //free the old pointer
   free(memPointer[lineIndex]);
+  //replace with the new ammount of memory needed
+  //making new blocks is probably what causes the crashing if replaceing too many things
   memPointer[lineIndex] = (char *)malloc(newLineLength*sizeof(char));
   if(memPointer[lineIndex] == NULL) printf("ERROR: Failed to allocate memPointer[%i]\n", lineIndex);
   strcpy(memPointer[lineIndex], newLine);
